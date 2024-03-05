f5c83fd3b4c398a3c7b209675f8c37869cdb8f21
suplazyboy <2369523736@qq.com>
2024-03-05 23:31:14 +0800

    计算了经过拟合曲面后各个点的坐标

--- Fitting.m
+++ Fitting.m
@@ -1,7 +1,8 @@
 clear all; close all; clc;
 format longE
 formatSpec = '%.15f';
-M = 150; N = 50;
+M = 150; N = 50;dz = 0.3;
+n1 = 1; n2 = 1.51; n3 = 1;
 x0 = 0; y0 = 0; z0 = 0;
 tic
 %% Params need to confirm
@@ -70,6 +71,99 @@ for w = 25  %: d : an
             surf(xxx1, yyy1, zzz1); hold on
             surf(xxx2, yyy2, zzz2);
             colormap('copper'); shading interp; %view(0,0)
+
+            for j = 1 : M
+                for i = 1 : N
+                    y0 = dz * abs(sin(atan(Z0(i, j) / X0(i, j))));
+                    line([x0, X0(i, j)], [y0, Y0(i, j)], [z0, Z0(j, i)], 'color', 'r'); hold on;
+                    I1{j, i}(1) = X0(j, i) - x0;
+                    I1{j, i}(2) = Y0(j, i) - y0;
+                    I1{j, i}(3) = Z0(j, i) - z0;
+                    I1{j, i} = normr(I1{j, i});
+
+                    %yi表示的是第一个镜上的点
+                    options_1=optimset('tolfun',1e-6,'Display','off');
+                    fun_1=@(yi)[(yi(3)-z0).*I1{j,i}(1)./I1{j,i}(3)+x0-yi(1);...
+                        (yi(3)-z0).*I1{j,i}(2)./I1{j,i}(3)+y0-yi(2);...
+                        p(1) + p(2).*yi(1) + p(3).*yi(3) + p(4).*yi(1).^2 + p(5).*yi(1).*yi(3) + p(6).*yi(3).^2 + p(7).*yi(1).^3 + p(8).*yi(1).^2.*yi(3)+...
+                        p(9).*yi(1).*yi(3).^2 + p(10).*yi(3).^3 + p(11).*yi(1).^4 + p(12).*yi(1).^3.*yi(3) + p(13).*yi(1).^2.*yi(3).^2+...
+                        p(14).*yi(1).*yi(3).^3 + p(15).*yi(3).^4 + p(16).*yi(1).^5 + p(17).*yi(1).^4.*yi(3) + p(18).*yi(1).^3.*yi(3).^2+...
+                        p(19).*yi(1).^2.*yi(3).^3 + p(20).*yi(1).*yi(3).^4 + p(21).*yi(3).^5-yi(2)];
+                    yi = fsolve(fun_1, [xx1(j, i); yy1(j, i); zz1(j, i)], options_1);
+                    X1(j, i) = yi(1); Y1(j, i) = yi(2); Z1(j, i) = yi(3);
+                    line([X0(j, i), x1(j, i)], [Y0(j, i), y1(j, i)], [Z0(j, i), z1(j, i)], 'linewidth', 1, 'color', 'r'); hold on
+                    %N1表示的是第一个镜上的法向矢量
+                    syms x y z
+                    F1 = p(1) + p(2) .* x + p(3) .* z + p(4) .* x .^ 2 + p(5) .* x .* z + p(6) .* z .^ 2 + p(7) .* x .^ 3 + p(8) .* x .^ 2 .* z + ...
+                        p(9) .* x .* z .^ 2 + p(10) .* z .^ 3 + p(11) .* x .^ 4 + p(12) .* x .^ 3 .* z + p(13) .* x .^ 2 .* z .^ 2 + ...
+                        p(14) .* x .* z .^ 3 + p(15) .* z .^ 4 + p(16) .* x .^ 5 + p(17) .* x .^ 4 .* z + p(18) .* x .^ 3 .* z .^ 2 + ...
+                        p(19) .* x .^ 2 .* z .^ 3 + p(20) .* x .* z .^ 4 + p(21) .* z .^ 5 - y;
+                    Fx1 = diff(F1, x);
+                    Fz1 = diff(F1, z);
+                    x = yi(1); z = yi(3);
+                    N1{j, i}(1) = double(eval(Fx1));
+                    N1{j, i}(2) = 1;
+                    N1{j, i}(3) = double(eval(Fz1));
+                    N1{j, i} = normr(N1{j, i});
+
+                    %er表示的是第一个镜的出射光线矢量
+                    options_2=optimset('tolfun',1e-6,'Display','off');
+                    fun_2=@(er)(n2 * er - n1 * I1{j, i} - (n2 * dot(N1{j, i}, er) - n1 * dot(N1{j, i}, I1{j, i})) .* N1);
+                    er=fsolve(fun_2,[x2(j,i)-x1(j,i) y2(j,i)-y1(j,i) z2(j,i)-z1(j,i)],options_2);
+                    O1{j, i}(1) = er(1); O1{j, i}(2) = er(2); O1{j, i}(3) = er(3); %次镜的出射光线矢�?
+                    I2{j, i} = normr(O1{j, i})
+                end 
+            end
+
+            for j = 1 : M
+                for i = 1 : N
+                    %san表示的是第二个镜上的点
+                    options_3 = optimset('TolFun', 1e-6, 'Display', 'off');
+                    fun_3 = @(san)[(san(1) - x1(j, i)) .* I2{j, i}(3) ./ I2{j, i}(1) + z1(j, i) - san(3); ...
+                        (san(1) - x1(j, i)) .* I2{j, i}(2) ./ I2{j, i}(1) + y1(j, i) - san(2); ...
+                        q(1) + q(2) .* san(1) + q(3) .* san(3) + q(4) .* san(1) .^ 2 + q(5) .* san(1) .* san(3) + q(6) .* san(3) .^ 2 + q(7) .* san(1) .^ 3 + q(8) .* san(1) .^ 2 .* san(3) + ...
+                        q(9) .* san(1) .* san(3) .^ 2 + q(10) .* san(3) .^ 3 + q(11) .* san(1) .^ 4 + q(12) .* san(1) .^ 3 .* san(3) + q(13) .* san(1) .^ 2 .* san(3) .^ 2 + ...
+                        q(14) .* san(1) .* san(3) .^ 3 + q(15) .* san(3) .^ 4 + q(16) .* san(1) .^ 5 + q(17) .* san(1) .^ 4 .* san(3) + q(18) .* san(1) .^ 3 .* san(3) .^ 2 + ...
+                        q(19) .* san(1) .^ 2 .* san(3) .^ 3 + q(20) .* san(1) .* san(3) .^ 4 + q(21) .* san(3) .^ 5 - san(2)];
+                    san = fsolve(fun_3, [xx2(j, i); yy2(j, i); zz2(j, i)], options_3);
+                    X2(j, i) = san(1); Y2(j, i) = san(2); Z2(j, i) = san(3);
+                    line([X1(j, i), X2(j, i)], [Y1(j, i), Y2(j, i)], [Z1(j, i), Z2(j, i)], 'linewidth', 1, 'color', 'g'); hold on
+                    %N2表示的是第二个镜上的法向矢量
+                    syms x y z
+                    F1 = q(1) + q(2) .* x + q(3) .* z + q(4) .* x .^ 2 + q(5) .* x .* z + q(6) .* z .^ 2 + q(7) .* x .^ 3 + q(8) .* x .^ 2 .* z + ...
+                        q(9) .* x .* z .^ 2 + q(10) .* z .^ 3 + q(11) .* x .^ 4 + q(12) .* x .^ 3 .* z + q(13) .* x .^ 2 .* z .^ 2 + ...
+                        q(14) .* x .* z .^ 3 + q(15) .* z .^ 4 + q(16) .* x .^ 5 + q(17) .* x .^ 4 .* z + q(18) .* x .^ 3 .* z .^ 2 + ...
+                        q(19) .* x .^ 2 .* z .^ 3 + q(20) .* x .* z .^ 4 + q(21) .* z .^ 5 - y;
+                    Fx1 = diff(F1, x);
+                    Fz1 = diff(F1, z);
+                    N2{j, i}(1) = double(eval(Fx1));
+                    N2{j, i}(3) = double(eval(Fz1));
+                    N2{j, i}(2) = 1;
+                    N2{j, i} = normr(N2{j, i});
+
+                    %si表示的是主镜的出射光线矢量
+                    options_4=optimset('tolfun',1e-6,'Display','off');
+                    
+                    fun_4 = @(si)(n3 * si - n2 * I2{j, i} - (n3 * dot(N2{j, i}, si) - n3 * dot(N2{j, i}, I2{j, i})) .* N2);
+                    si = fsolve(fun_4,[X3(j,i)-x2(j,i) Y3(j,i)-y2(j,i) Z3(j,i)-z2(j,i)],options_2);
+                    O2{j,i}(1)=si(1);O2{j,i}(2)=si(2);O2{j,i}(3)=si(3);%主镜的出射光线矢量
+                    I3{j, i} = normr(O2{j,i});
+                    %计算发散角
+                    A3 = [0, 1, 0];
+                    alpha_mrad(j, i) = 1000 .* acos(dot(O2{j, i}, A3) / norm(O2{j, i}, 2) / norm(A3, 2));
+
+                    %计算出射面上的点
+                    options_5 = optimset('tolfun', 1e-6, 'Display', 'off');
+                    fun_5 = @(wu)[
+                        (Y3(j, i) - Y2(j, i)) .* O2{j, i}(1) ./ O2{j, i}(2) + X2(j, i) - wu(1); ...
+                        (Y3(j, i) - Y2(j, i)) .* O2{j, i}(3) ./ O2{j, i}(2) + Z2(j, i) - wu(2)
+                    ];
+                    wu = fsolve(fun_5, [X3(j, i) Z3(j, i)], options_3);
+                    x3(j, i) = wu(1); y3(j, i) = Y3(j, i); z3(j, i) = wu(2);
+                    line([X2(j, i), x3(j, i)], [Y2(j, i), y3(j, i)], [Z2(j, i), z3(j, i)], 'linewidth', 1, 'color', 'b'); hold on
+                end
+            end
+
         end
     end
 end
